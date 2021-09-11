9b048d99ee9c363e748af9bfbd4ca8286a2a4de7
kant1310 <canhtv.ee@gmail.com>
2021-09-11 16:53:31 +0700

    chat fragment update from dell5520 09092021

--- app/src/main/java/com/canhtv/ee/firebasechatapp/ui/ChatFragment.kt
+++ app/src/main/java/com/canhtv/ee/firebasechatapp/ui/ChatFragment.kt
@@ -1,5 +1,6 @@
 package com.canhtv.ee.firebasechatapp.ui
 
+import android.net.wifi.hotspot2.pps.Credential
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -7,6 +8,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.canhtv.ee.firebasechatapp.databinding.FragmentChatBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
@@ -30,22 +33,28 @@ class ChatFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val database = Firebase.database
-        val msRef = database.getReference("users")
-
-        msRef.addValueEventListener(object: ValueEventListener {
-
-            override fun onDataChange(snapshot: DataSnapshot) {
-                val value = snapshot.value
-                Log.d("TAG", "Value is: $value")
-                msRef.setValue(value)
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                Log.w("TAG", "Failed to read value.", error.toException())
-            }
-
-        })
+//        val database = Firebase.database
+//        val msRef = database.getReference("users")
+//
+//        msRef.addValueEventListener(object: ValueEventListener {
+//            override fun onDataChange(snapshot: DataSnapshot) {
+//                val value = snapshot.value
+//                Log.d("TAG", "Value is: $value")
+//                msRef.setValue(value)
+//            }
+//            override fun onCancelled(error: DatabaseError) {
+//                Log.w("TAG", "Failed to read value.", error.toException())
+//            }
+//        })
+//
+//        val currentUser = FirebaseAuth.getInstance().currentUser
+//        val hashMap = HashMap<String, String>()
+//        with(hashMap) {
+//            put(currentUser!!.uid, currentUser.email!!)
+//        }
+//        msRef.setValue(hashMap)
+//        msRef.child(currentUser!!.uid).setValue(currentUser)
+//        msRef.child(currentUser!!.uid).setValue(currentUser.email)
     }
 
     override fun onDestroy() {
